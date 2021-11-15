b0c4f6771caab5044a5dc09d30303f0e72a19676
Katherine Rocha <krocha@comcast.net>
2021-11-14 18:07:29 -0700

    Fix Format for Manage

--- src/Manage.cpp
+++ src/Manage.cpp
@@ -6,15 +6,13 @@
 #include "Manage.hpp"
 
 namespace RDS {
-template <typename T> void
-Manage_UX(std::vector<T>& items) {
+template <typename T>
+void Manage_UX(std::vector<T>& items) {}
 
-}
-	
-void Manage(std::vector<RDS::Radio>& radios){
+void Manage(std::vector<RDS::Radio>& radios) {
     Manage_UX(radios);
 }
-void Manage(std::vector<RDS::Person>& people){
-	Manage_UX(radios);
+void Manage(std::vector<RDS::Person>& people) {
+    Manage_UX(radios);
 }
 }  // namespace RDS
